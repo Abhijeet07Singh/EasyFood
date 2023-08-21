06fd19577b0b719d1f21e7a79416857e1cb36b2f
Abhijeet07Singh <abhijeet.singh@iiitg.ac.in>
2023-08-22 01:14:22 +0530

    initail files added

---
+++ src/Components/Header.js
@@ -0,0 +1,56 @@
+import {
+  BarChart,
+  SearchRounded,
+  ShoppingCartRounded,
+} from "@mui/icons-material";
+import React, { useEffect } from "react";
+import { useStateValue } from "./StateProvider";
+
+function Header() {
+  const [{ cart }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const toggleIcon = document.querySelector(".toggleMenu");
+    toggleIcon.addEventListener("click", () => {
+      document.querySelector(".rightMenu").classList.toggle("active");
+    });
+  }, []);
+
+  return (
+    <header>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/restaurantapp-6c901.appspot.com/o/Images%2Fchef1.png?alt=media&token=3ca80625-db8e-4e14-b77c-0f674eebe304"
+        alt=""
+        className="logo"
+      />
+
+      <div className="inputBox">
+        <SearchRounded className="searchIcon" />
+        <input type="text" placeholder="Search" />
+      </div>
+
+      <div className="shoppingCart">
+        <ShoppingCartRounded className="cart" />
+        <div className={`${!cart ? "noCartItem" : "cart_content"}`}>
+          <p>{cart ? cart.length : ""}</p>
+        </div>
+      </div>
+
+      <div className="profileContainer">
+        <div className="imgBox">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/restaurantapp-6c901.appspot.com/o/Images%2Fpassport_photo-fotor-bg-remover-20230731181532.png?alt=media&token=ff6f9e26-7318-4055-8dfb-cb4e861c1111"
+            alt=""
+          />
+        </div>
+        <h2 className="userName">Abhijeet</h2>
+      </div>
+
+      <div className="toggleMenu">
+        <BarChart className="toggleIcon" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
